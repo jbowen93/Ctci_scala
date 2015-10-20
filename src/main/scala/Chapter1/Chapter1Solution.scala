36263476a33de52036932d72152bd8720da3bedc
jbowen93 <jish.bowen@gmail.com>
2015-10-20 13:01:54 -0600

    Added imperative solution and test for problem 1.1

--- src/main/scala/Chapter1/Chapter1Solution.scala
+++ src/main/scala/Chapter1/Chapter1Solution.scala
@@ -2,8 +2,23 @@ package Chapter1
 
 class Chapter1Solution {
 
-  def isUniqueChar(str: String) = {
+  def isUniqueCharFunctional(str: String) = {
     str.distinct == str
+  }
+
+  def isUniqueChar(str: String) = {
+    var ret = true
+    if (str.length > 256) {
+      ret = false
+    }
+    val charSet = new Array[Boolean](256)
+    for((value,index) <- str.view.zipWithIndex) {
+      if (charSet(value)) {
+        ret = false
+      }
+      charSet(value) = true
+    }
+    ret
   }
 
 }
--- src/test/scala/Chapter1/Chapter1Solution$Test.scala
+++ src/test/scala/Chapter1/Chapter1Solution$Test.scala
@@ -6,12 +6,27 @@ class Chapter1Solution$Test extends FunSuite {
 
   val testObject = new Chapter1Solution
 
-  test("IsUniqueChar called on string with repeated chars") {
-    assert(testObject.isUniqueChar("titties") === false)
+  //Question 1.1 Functional Tests
+  test("Functional method called on string with repeated chars") {
+    assert(testObject.isUniqueCharFunctional("aabbcc") === false)
   }
 
-  test("isUniqueChar called on string with unique chars") {
+  test("Functional method called on string with unique chars") {
+    assert(testObject.isUniqueCharFunctional("abcdefg") === true)
+  }
+
+  //Question 1.1 Imperative Tests
+  test("Imperative method called on string with repeated chars") {
+    assert(testObject.isUniqueChar("aabbcc") === false)
+  }
+
+  test("Imperative method called on string with unique chars") {
     assert(testObject.isUniqueChar("abcdefg") === true)
   }
 
+  //Question 1.2 Functional Tests
+
+  //Question 1.3 Tests
+
+  //etc
 }
